3a1a0a1f6c1c5404145e5be300f94694d1ae3458
Aks <abhinay302@gmail.com>
2020-07-14 17:47:47 +0530

    add option to choose date via calender

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
+  before_action :authenticate_user!
+
   def index
+    @asset = {}
+    @assets[:stocks] = current_user.stocks
   end
 
   def terms
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,13 +1,13 @@
 import { Application } from "stimulus"
-
-// import Flatpickr
-import Flatpickr from 'stimulus-flatpickr'
-
 import { definitionsFromContext } from "stimulus/webpack-helpers"
 
 const application = Application.start()
 const context = require.context(".", true, /\.js$/)
 application.load(definitionsFromContext(context))
+
+// import Flatpickr
+import Flatpickr from 'stimulus-flatpickr'
+require("flatpickr/dist/flatpickr.css")
 
 // Manually register Flatpickr as a stimulus controller
 application.register('flatpickr', Flatpickr)
